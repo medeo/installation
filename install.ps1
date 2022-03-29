06054cd9d40ae60700f1a3837774fc69a0225fb0
Yaya <yacine@medeo-health.com>
2022-03-29 16:14:07 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -26,5 +26,4 @@ $Driver = "DriverWindows.zip"
 
 Expand-Archive "$LocalTempDir\$Driver" -DestinationPath "$LocalTempDir\"
 
-Get-ChildItem "c:\medeoInstallation\windrv" -Recurse -Filter "*.inf" | 
-ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install 
+Get-ChildItem "c:\medeoInstallation\windrv" -Recurse -Filter "*inf" | ForEach-Object { PNPUtil.exe /add-driver $_.FullName /install } 
