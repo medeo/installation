8a99ca4ad0ac09889185f026603a9eb300d35f19
Yaya <yacine@medeo-health.com>
2022-03-29 15:48:07 +0200

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,21 @@
+Write-Host "`n"
+Write-Host " ____________________________________________________________________________ " -ForegroundColor Red 
+Write-Host "|                                                                            |" -ForegroundColor Red 
+Write-Host "|    "  -ForegroundColor Red -NoNewline; Write-Host "                  " -ForegroundColor Green -NoNewline; Write-Host "                                                      |" -ForegroundColor Red    
+Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "          __    __     ______     _____     ______     ______            " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
+Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "         /\ '-./  \   /\  ___\   /\  __-.  /\  ___\   /\  __ \           " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
+Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "         \ \ \-./\ \  \ \  __\   \ \ \/\ \ \ \  __\   \ \ \/\ \          " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
+Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "          \ \_\ \ \_\  \ \_____\  \ \____-  \ \_____\  \ \_____\         " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
+Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "           \/_/  \/_/   \/_____/   \/____/   \/_____/   \/_____/         " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
+Write-Host "|                                                                            |" -ForegroundColor Red 
+Write-Host "|                                 Version 2022.0                             |" -ForegroundColor Red 
+Write-Host "|                             contact@medeo-health.com                       |" -ForegroundColor Red 
+Write-Host "|____________________________________________________________________________|" -ForegroundColor Red 
+Write-Host "|                                                                            |" -ForegroundColor Red 
+Write-Host "|                                   Created by                               |" -ForegroundColor Red 
+Write-Host "|                                     Medeo                                  |" -ForegroundColor Red 
+Write-Host "|____________________________________________________________________________|" -ForegroundColor Red 
+
+New-Item -Path "c:\" -Name "medeoInstallation" -ItemType "directory"
+url = "https://kligo.s3.eu-central-1.amazonaws.com/USB-Signed-Win-Drv.zip"; $LocalTempDir = "c:\medeoInstallation"; $Driver = "DriverWindows.zip"; (New-Object System.Net.WebClient).DownloadFile($url, "$LocalTempDir\$Driver")
+Expand-Archive "$LocalTempDir\$Driver" -DestinationPath "$LocalTempDir\"
