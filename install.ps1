dd9b9a03a3481010c5d80ae3559ecb9781a7cf9d
Yaya <yacine@medeo-health.com>
2022-03-29 19:00:55 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -47,3 +47,39 @@ $AnyDesk = "AnyDesk.exe"
 $ChromeInstaller = "ChromeInstaller.exe"; 
 $urlChrome = "http://dl.google.com/chrome/install/chrome_installer.exe"
 (new-object System.Net.WebClient).DownloadFile($urlChrome, "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; 
+
+#URLMedeo
+$urlURLMEDEO = "https://raw.githubusercontent.com/medeo/installation/main/kiosk.csv"
+$URLMEDEO = "kiosk.csv"
+(New-Object System.Net.WebClient).DownloadFile($urlURLMEDEO, "$LocalTempDir\$URLMEDEO")
+
+$WShell = New-Object -comObject WScript.Shell
+$Shortcut = $WShell.CreateShortcut("$Home\Desktop\Medeo.lnk")
+$Shortcut.TargetPath = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+$Shortcut.Arguments = "-kiosk https://medeo.care"
+$Shortcut.Save()
+
+$destination = 'F:\destination'
+$chromeroot = 'C:\Program Files (x86)\Google\Chrome'
+$workingdir = 'C:\script'
+$input = Import-Csv .\kiosk.csv
+ 
+function Create-Shortcut
+{
+$Shell = New-Object -ComObject ("WScript.Shell")
+$ShortCut = $Shell.CreateShortcut("$destination\$URLTitle.lnk")
+$ShortCut.TargetPath="$chromeroot\Application\chrome.exe"
+$ShortCut.Arguments="--kiosk $URL"
+$ShortCut.WorkingDirectory = "$chromeroot\Application";
+$ShortCut.WindowStyle = 1;
+$ShortCut.IconLocation = "$chromeroot\Application\chrome.exe, 0";
+$ShortCut.Description = "";
+$ShortCut.Save()
+}
+ 
+ForEach ($item in $input){
+    $URLTitle = $($item.name)
+    $URL = $($item.url)
+    Create-Shortcut
+    }
+
