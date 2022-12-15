9b4e8b6ca10c132623b68c4cc456b03e829f9c84
Yaya <yacine@medeo-health.com>
2022-12-15 19:52:35 +0100

    Update chrome-extension.ps1

--- chrome-extension.ps1
+++ chrome-extension.ps1
@@ -16,3 +16,32 @@ Write-Host "|                                                                   
 Write-Host "|                                   Created by                               |" -ForegroundColor Red 
 Write-Host "|                                     Medeo                                  |" -ForegroundColor Red 
 Write-Host "|____________________________________________________________________________|" -ForegroundColor Red 
+
+
+#Set variables as input for the script
+$KeyPath = "HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist"
+$KeyName = "1"
+$KeyType = "String"
+$KeyValue = "ilbdbafpgbnlnmlpojeaiedhocikipjm;https://clients2.google.com/service/update2/crx"
+
+#Verify if the registry path already exists
+if(!(Test-Path $KeyPath)) {
+    try {
+        #Create registry path
+        New-Item -Path $KeyPath -ItemType RegistryKey -Force -ErrorAction Stop
+    }
+    catch {
+        Write-Output "FAILED to create the registry path"
+    }
+}
+
+#Verify if the registry key already exists
+if(!((Get-ItemProperty $KeyPath).$KeyName)) {
+    try {
+        #Create registry key 
+        New-ItemProperty -Path $KeyPath -Name $KeyName -PropertyType $KeyType -Value $KeyValue
+    }
+    catch {
+        Write-Output "FAILED to create the registry key"
+    }
+}
