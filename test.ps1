32eaa9a82e49b09399470d4e78c643702f857ff9
Yaya <yacine@medeo-health.com>
2022-12-15 20:12:10 +0100

    Delete test.ps1

--- test.ps1
+++
@@ -1,9 +0,0 @@
-New-Item -Path "c:\" -Name "medeoInstallation" -ItemType "directory"
-
-$LocalTempDir = "c:\medeoInstallation"
-
-$urlDriver = "https://raw.githubusercontent.com/medeo/installation/main/New-ChromeExtension.ps1"
-$ChromeExtension = "New-ChromeExtension.ps1"
-(New-Object System.Net.WebClient).DownloadFile($urlDriver, "$LocalTempDir\$ChromeExtension")
-
-New-ChromeExtension -ExtensionID 'ilbdbafpgbnlnmlpojeaiedhocikipjm'
