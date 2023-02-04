8685e71c708196077123e01a5520232aca40ad8b
Yaya <thehaker42@yahoo.fr>
2023-02-04 21:37:37 +0100

    update

--- installation.ps1
+++ installation.ps1
@@ -11,7 +11,7 @@ while ($continue) {
     Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "          \ \_\ \ \_\  \ \_____\  \ \____-  \ \_____\  \ \_____\         " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
     Write-Host "|  "  -ForegroundColor Red -NoNewline; Write-Host "           \/_/  \/_/   \/_____/   \/____/   \/_____/   \/_____/         " -ForegroundColor Green -NoNewline; Write-Host " |" -ForegroundColor Red  
     Write-Host "|                                                                            |" -ForegroundColor Red 
-    Write-Host "|                                 Version 2022.0                             |" -ForegroundColor Red 
+    Write-Host "|                                 Version 2023.2                             |" -ForegroundColor Red 
     Write-Host "|                             contact@medeo-health.com                       |" -ForegroundColor Red 
     Write-Host "|____________________________________________________________________________|" -ForegroundColor Red 
     Write-Host "|                                                                            |" -ForegroundColor Red 
@@ -29,6 +29,8 @@ while ($continue) {
     write-host "3. Installer Kligo + Driver + Extension Chrome"
     write-host "4. Installer Extension Chrome"
     write-host "5. Installer task pour cohabitation avec VitalZen"
+    write-host "6. Désinstaller Extension Chrome"
+    write-host "7. Préparation PC MEDEO"
     write-host "q. exit"
     write-host "--------------------------------------------------------"
     $choix = read-host "faire un choix "
@@ -39,90 +41,113 @@ while ($continue) {
     }
     $LocalTempDir = "c:\medeoInstallation"
 
+    function Install-Kligo {
+        #Kligo
+        $urlKligo = "https://kligo-rollouts112226-dev.s3.eu-west-1.amazonaws.com/staged/Kligo+Setup+6.0.0-develop.15.exe"
+        $Kligo = "Kligo.exe"
+
+        filter Get-FileSize {
+            "{0:N2} {1}" -f $(
+                if ($_ -lt 1kb) { $_, 'Bytes' }
+                elseif ($_ -lt 1mb) { ($_ / 1kb), 'KB' }
+                elseif ($_ -lt 1gb) { ($_ / 1mb), 'MB' }
+                elseif ($_ -lt 1tb) { ($_ / 1gb), 'GB' }
+                elseif ($_ -lt 1pb) { ($_ / 1tb), 'TB' }
+                else { ($_ / 1pb), 'PB' }
+            )
+        }function Get-FileVB {
+            param(
+                [Parameter(Mandatory = $true)]
+                $urlKligo, 
+                $destinationFolder = "$LocalTempDir", 
+                [switch]$includeStats
+    
+            )
+            Add-Type –AssemblyName Microsoft.VisualBasic
+            #resolve potential redirect
+            $response = [System.Net.WebRequest]::Create($url).GetResponse()
+            $urlKligo = $response.ResponseUri.OriginalString
+            $response.Close()
+            $destination = Join-Path $destinationFolder ($Kligo | Split-Path –Leaf)
+            $net = New-Object Microsoft.VisualBasic.Devices.Network
+            $start = Get-Date
+            #signature DownloadFile(url, destination, username, password, [bool]showUI, [int]connecdtionTimeOutInMS,[Microsoft.VisualBasic.FileIO.UICancelOption]OnUserCancel)
+            $net.DownloadFile($url, $destination, '', '', $true, 500, [Microsoft.VisualBasic.FileIO.UICancelOption]::DoNothing )
+            $elapsed = ((Get-Date) – $start).ToString('hh\:mm\:ss')
+            $totalSize = (Get-Item $destination).Length | Get-FileSize
+            if ($includeStats.IsPresent) {
+                [PSCustomObject]@{Name = $MyInvocation.MyCommand; TotalSize = $totalSize; Time = $elapsed }
+            }
+            Get-Item $destination | Unblock-File
+        }
+    
+        Get-FileVB $urlKligo -includeStats
+        Start-Process -Wait -FilePath "$LocalTempDir\$Kligo" -ArgumentList "/silent /install"
+
+    }
+
+    function Install-Extension {
+        $regLocation = 'Software\Policies\Google\Chrome\ExtensionInstallForcelist'
+        # Each extension if you want to force install more than 1 extension needs its own key #
+
+        If (!(Test-Path "HKLM:\$regLocation")) {
+            Write-Verbose -Message "No Registry Path, setting count to: 0"
+            [int]$Count = 0
+            Write-Verbose -Message "Count is now $Count" 
+            New-Item -Path "HKLM:\$regLocation" -Force
+
+        }
+
+        Else {
+            Write-Verbose -Message "Keys found, counting them..."
+            [int]$Count = (Get-Item "HKLM:\$regLocation").Count
+            Write-Verbose -Message "Count is now $Count"
+        }
+
+
+        $regKey = $Count + 1
+        Write-Verbose -Message "Creating reg key with value $regKey"
+
+        $regData = "ilbdbafpgbnlnmlpojeaiedhocikipjm;https://clients2.google.com/service/update2/crx"
+        New-ItemProperty -Path "HKLM:\$regLocation" -Name $regKey -Value $regData -PropertyType STRING -Force
+    }
+
+    function Install-Driver {
+        $urlDriver = "https://kligo.s3.eu-central-1.amazonaws.com/USB-Signed-Win-Drv.zip"
+        $Driver = "DriverWindows.zip"
+        (New-Object System.Net.WebClient).DownloadFile($urlDriver, "$LocalTempDir\$Driver")
+        Expand-Archive "$LocalTempDir\$Driver" -DestinationPath "$LocalTempDir\"
+        pnputil /add-driver "$LocalTempDir\windrv\*inf" /install
+    }
+    
+
     switch ($choix) {
         1 {
-            #Driver
-            $urlDriver = "https://kligo.s3.eu-central-1.amazonaws.com/USB-Signed-Win-Drv.zip"
-            $Driver = "DriverWindows.zip"
-            (New-Object System.Net.WebClient).DownloadFile($urlDriver, "$LocalTempDir\$Driver")
-            Expand-Archive "$LocalTempDir\$Driver" -DestinationPath "$LocalTempDir\"
-            pnputil /add-driver "$LocalTempDir\windrv\*inf" /install
+            #Installation du Driver
+            Install-Driver
 
-            #Kligo
-            $urlKligo = "https://kligo-rollouts112226-dev.s3.eu-west-1.amazonaws.com/staged/Kligo+Setup+6.0.0-develop.15.exe"
-            $Kligo = "Kligo.exe"
-            (New-Object System.Net.WebClient).DownloadFile($urlKligo, "$LocalTempDir\$Kligo"); & "$LocalTempDir\$Kligo" /silent /install;
+            #Installation de Kligo
+            Install-Kligo
         }
         2 { 
-            #Kligo
-            $urlKligo = "https://kligo-rollouts112226-dev.s3.eu-west-1.amazonaws.com/staged/Kligo+Setup+6.0.0-develop.15.exe"
-            $Kligo = "Kligo.exe"
-            (New-Object System.Net.WebClient).DownloadFile($urlKligo, "$LocalTempDir\$Kligo"); & "$LocalTempDir\$Kligo" /silent /install;   
+            #Installation de Kligo
+            Install-Kligo
+            clear   
         }
         3 {
-            #Driver
-            $urlDriver = "https://kligo.s3.eu-central-1.amazonaws.com/USB-Signed-Win-Drv.zip"
-            $Driver = "DriverWindows.zip"
-            (New-Object System.Net.WebClient).DownloadFile($urlDriver, "$LocalTempDir\$Driver")
-            Expand-Archive "$LocalTempDir\$Driver" -DestinationPath "$LocalTempDir\"
-            pnputil /add-driver "$LocalTempDir\windrv\*inf" /install
+            #Installation du Driver
+            Install-Driver
 
-            #Kligo
-            $urlKligo = "https://kligo-rollouts112226-dev.s3.eu-west-1.amazonaws.com/staged/Kligo+Setup+6.0.0-develop.15.exe"
-            $Kligo = "Kligo.exe"
-            (New-Object System.Net.WebClient).DownloadFile($urlKligo, "$LocalTempDir\$Kligo"); & "$LocalTempDir\$Kligo" /silent /install;
+            #Installation de Kligo
+            Install-Kligo
 
-            #Install Kligo Chrome extension
-            $regLocation = 'Software\Policies\Google\Chrome\ExtensionInstallForcelist'
-            # Each extension if you want to force install more than 1 extension needs its own key #
-
-            If (!(Test-Path "HKLM:\$regLocation")) {
-                Write-Verbose -Message "No Registry Path, setting count to: 0"
-                [int]$Count = 0
-                Write-Verbose -Message "Count is now $Count" 
-                New-Item -Path "HKLM:\$regLocation" -Force
-
-            }
-
-            Else {
-                Write-Verbose -Message "Keys found, counting them..."
-                [int]$Count = (Get-Item "HKLM:\$regLocation").Count
-                Write-Verbose -Message "Count is now $Count"
-            }
-
-
-            $regKey = $Count + 1
-            Write-Verbose -Message "Creating reg key with value $regKey"
-
-            $regData = "ilbdbafpgbnlnmlpojeaiedhocikipjm;https://clients2.google.com/service/update2/crx"
-            New-ItemProperty -Path "HKLM:\$regLocation" -Name $regKey -Value $regData -PropertyType STRING -Force
+            #Installation Chrome extension
+            Install-Extension
 
         }
         4 {
-            #Install Kligo Chrome extension
-            $regLocation = 'Software\Policies\Google\Chrome\ExtensionInstallForcelist'
-            # Each extension if you want to force install more than 1 extension needs its own key #
-
-            If (!(Test-Path "HKLM:\$regLocation")) {
-                Write-Verbose -Message "No Registry Path, setting count to: 0"
-                [int]$Count = 0
-                Write-Verbose -Message "Count is now $Count" 
-                New-Item -Path "HKLM:\$regLocation" -Force
-
-            }
-
-            Else {
-                Write-Verbose -Message "Keys found, counting them..."
-                [int]$Count = (Get-Item "HKLM:\$regLocation").Count
-                Write-Verbose -Message "Count is now $Count"
-            }
-
-
-            $regKey = $Count + 1
-            Write-Verbose -Message "Creating reg key with value $regKey"
-
-            $regData = "ilbdbafpgbnlnmlpojeaiedhocikipjm;https://clients2.google.com/service/update2/crx"
-            New-ItemProperty -Path "HKLM:\$regLocation" -Name $regKey -Value $regData -PropertyType STRING -Force
+            #Installation Chrome extension
+            Install-Extension
         }
         5 {
             #VITALZEN
@@ -138,14 +163,44 @@ while ($continue) {
             
             Register-ScheduledTask -TaskName "LAUNCHER_VITALZEN" -Xml (Get-Content "$LocalTempDir\$VZL" | Out-String) -Force    
             Register-ScheduledTask -TaskName "REMOVER_VITALZEN" -Xml (Get-Content "$LocalTempDir\$VZR" | Out-String) -Force    
-            taskmgr
+            taskschd.msc
 
         }
         6 {
             #Désinstallation de l'extension Chrome
+            param(
+                $KeyPath = ""
+            )
+            Set-ItemProperty -Path "HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist" -Name Lastkey -Value $KeyPath -Type String -Force
+            Start-Process "regedit.exe" -args "-m"
+
+        }
+        7 {
+            #Préparation PC MEDEO
+            powercfg -change -monitor-timeout-ac 0
+            powercfg -change -monitor-timeout-dc 0
+            powercfg -change -standby-timeout-ac 0
+            powercfg -change -standby-timeout-dc 0
+
+            #Favoris
+            $LocalTempDirChrome = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default"
+            $urlFavoris = "https://raw.githubusercontent.com/medeo/installation/main/documents/Bookmarks"
+            $Favoris = "Bookmarks"
+            (New-Object System.Net.WebClient).DownloadFile($urlFavoris, "$LocalTempDirChrome\$Favoris")
+
+            #Wallpaper
+            $urlWallpaper = "https://raw.githubusercontent.com/medeo/installation/main/documents/wallpaper.png"
+            $Wallpaper = "wallpaper.png"
+            (New-Object System.Net.WebClient).DownloadFile($urlWallpaper, "$LocalTempDir\$Wallpaper")
+            New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\" -Name "System"
+            New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "Wallpaper" -Value "$LocalTempDir\wallpaper.png"  -PropertyType "String"
+            New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "WallpaperStyle" -Value "4"  -PropertyType "String"
+            stop-process -name explorer -force
 
         }
         ‘q’ { $continue = $false }
         default { Write-Host "Choix invalide" -ForegroundColor Red }
     }
 }
+
+
